ce8ca3dba74373ab9b881bf11dc04bfc9c7692fd
Silverarrow144 <39146776+Silverarrow144@users.noreply.github.com>
2018-05-12 19:15:03 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "graphics.hpp"
 #include "dropDownBox.hpp"
 #include "inputBox.hpp"
@@ -7,13 +6,79 @@
 #include "widget.hpp"
 #include "window.hpp"
 #include "justText.hpp"
+#include "scene.hpp"
+
+#include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 using namespace genv;
 
+
+
+class mainWindow : public window
+{
+    public:
+    scene * fomenu;         /// F
+    scene * lovarda;        /// O
+    scene * legelo;         /// L
+    scene * versenypalya;   /// V
+    scene * naptar;         /// N
+    scene * bolt;           /// B
+    scene * menu;           /// M
+
+    void whichToDraw(char sceneTipusa)
+    {
+        switch(sceneTipusa)
+        {
+        case 'F':
+            //drawthis
+            break;
+        case 'O':
+            //drawthis
+            break;
+        case 'L':
+            //drawthis
+            break;
+        case 'V':
+            //drawthis
+            break;
+        case 'N':
+            //drawthis
+            break;
+        case 'B':
+            //drawthis
+            break;
+        }
+    }
+
+
+
+
+
+};
+
+
+
+
+
+
+/*void theWindow::fv(window *w) {
+    theWindow * mw = dynamic_cast<theWindow*>(w);
+    if (mw) {
+        mw->valami();
+    }
+}*/
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    event ev;
+    while (gin >> ev){
+    gout.open(640,480);
+    //theWindow *TheWindow = new theWindow;
+    //TheWindow->event_loop();
+    }
     return 0;
 }
